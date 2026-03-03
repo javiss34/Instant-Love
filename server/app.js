8d929af6afc061dfea15ae372db3d1a90329e069
javisr34 <javiersanchezsolera.alu@iespacomolla.es>
2026-03-03 20:43:51 +0100

    Añadido el .env

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ import outcomeRoutes from "./routes/outcomeRoutes.js";
 import reportRoutes from "./routes/reportRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
